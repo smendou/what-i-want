29bc3f9e3a86be0cd9f677e2c0d368046dc98c12
smendou <tmshorel@gmail.com>
2015-05-04 16:04:19 +0200

    friends

--- controllers/friends.js
+++ controllers/friends.js
@@ -79,6 +79,6 @@ exports.postSearch = function(req, res, next) {
 exports.addFriend = function(req, res, next) {
   User.requestFriend(req.user._id, req.params.friendid, function(err, friendships){
     //res.json(friendships);
-    res.redirect('friends/requested');
+    res.redirect('../requested');
   });
 };
