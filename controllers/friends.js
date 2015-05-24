e7cd77ddb26b5b2ef01269406b27279100c768b0
smendou <tmshorel@gmail.com>
2015-05-24 22:09:43 +0200

    wants

--- controllers/friends.js
+++ controllers/friends.js
@@ -54,7 +54,7 @@ exports.addFriend = function(req, res, next) {
 exports.rmFriend = function(req, res, next) {
   User.removeFriend(req.user._id, req.params.friendid, function(err, friendships){
     if(err){ return next(err); }
-    res.json(req.params);
+    res.json(req.user);
   });
 };
 
